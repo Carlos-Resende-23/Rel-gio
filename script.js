b784d8ba11e524c4651f14a9bee5c6f6c044b53b
Carlos Resende <resende.carlinhos123@gmail.com>
2026-05-04 20:05:13 -0300

    Add date + Style

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const horas = document.getElementById("horas")
 const minutos = document.getElementById("minutos")
 const segundos = document.getElementById("segundos")
+const data = document.getElementById("data")
+const diasSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
 
 const relogio = setInterval(function time() {
   let dateToday = new Date()
   let hr = dateToday.getHours()
   let min = dateToday.getMinutes()
   let s = dateToday.getSeconds()
+  let dia = dateToday.getDate()
+  let mes = dateToday.getMonth() + 1
+  let ano = dateToday.getFullYear()
+  let diaSemana = diasSemana[dateToday.getDay()]
 
   if (hr < 10) hr = "0" + hr
   if (min < 10) min = "0" + min
   if (s < 10) s = "0" + s
+  if (dia < 10) dia = "0" + dia
+  if (mes < 10) mes = "0" + mes
 
   horas.textContent = hr
   minutos.textContent = min
   segundos.textContent = s
+  data.textContent = `${diaSemana} - ${dia}/${mes}/${ano}`
 }, 1000)
